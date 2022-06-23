cdda17475786f33ed27400f45912674c4b268979
mdel <mdelefm@gmail.com>
2022-06-23 00:30:56 -0700

    Add featured properties on Home page
    added featured properties on home page

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import "./featured.css"
+
+function Featured() {
+  return (
+    <div className="featured">
+      <div className="featured__item">
+        <img src="https://cf.bstatic.com/xdata/images/city/square250/653240.webp?k=6b015a87c8443039a685038e97dd58dab6a8748078948b27cca7d1a5fcef308d&o=" alt="" className="featured__img"/>
+        <div className="featured__titles">
+          <h2>Dublin</h2>
+          <h3>123 properties</h3>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img src="https://cf.bstatic.com/xdata/images/city/square250/653240.webp?k=6b015a87c8443039a685038e97dd58dab6a8748078948b27cca7d1a5fcef308d&o=" alt="" className="featured__img" />
+        <div className="featured__titles">
+          <h2>Dublin</h2>
+          <h3>123 properties</h3>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img src="https://cf.bstatic.com/xdata/images/city/square250/653240.webp?k=6b015a87c8443039a685038e97dd58dab6a8748078948b27cca7d1a5fcef308d&o=" alt="" className="featured__img"/>
+        <div className="featured__titles">
+          <h2>Dublin</h2>
+          <h3>123 properties</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 
 import Header from '../../components/header/Header.jsx'
-import Navbar from '../../components/navbar/Navbar.jsx'
 import './home.css'
+import Featured from '../../components/featured/Featured.jsx'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <div className="home__container">
+        <Featured/>
+        <Featured/>
+      </div>
+
     </div>
   )
 }
